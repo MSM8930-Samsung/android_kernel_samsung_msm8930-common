510a5835fc4589fffa7327f4abe6dd080322f113
kostoulhs <MrKostoulhs@gmail.com>
2016-06-30 21:03:30 +0200

    Revert "Implement Kexec-hardboot"
    This reverts commit 455affa2a701808752501c7b3d2379c311440ea9.
    
    # Conflicts:
    #	arch/arm/configs/cyanogen_loganre_defconfig

--- arch/arm/mach-msm/board-loganre.c
+++ arch/arm/mach-msm/board-loganre.c
@@ -1982,16 +1982,6 @@ static void __init msm8930_reserve(void)
 
 static void __init msm8930_allocate_memory_regions(void)
 {
-#ifdef CONFIG_KEXEC_HARDBOOT
-    // Reserve space for hardboot page at the end of first system ram block
-    struct membank* bank = &meminfo.bank[0];
-    phys_addr_t start = bank->start + bank->size - SZ_1M;
-    int ret = memblock_remove(start, SZ_1M);
-    if(!ret)
-        pr_info("Hardboot page reserved at 0x%X\n", start);
-    else
-        pr_err("Failed to reserve space for hardboot page at 0x%X!\n", start);
-#endif
 	msm8930_allocate_fb_region();
 }
 
