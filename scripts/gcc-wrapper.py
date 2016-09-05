633adeca7ae5902cefd329ca6d466f1c6490da31
kostoulhs <MrKostoulhs@gmail.com>
2016-09-06 02:26:14 +0300

    Temp fix for compile errors

--- scripts/gcc-wrapper.py
+++ scripts/gcc-wrapper.py
@@ -69,6 +69,10 @@ allowed_warnings = set([
     "f_qdss.c:740",
     "mipi_novatek.c:569",
     "swab.h:34",
+    "svcauth_unix.c:531",
+    "cache.h:260",
+    "cpufreq_intelliactive.c:832",
+    "cpufreq_intelliactive.c:875",
  ])
 
 # Capture the name of the object file, can find it.
